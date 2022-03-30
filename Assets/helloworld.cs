7ab5aeeefef7618cf0eedc544ae3a768f4b034c2
pontunity <Pontus.Hogler@yh.nackademin.se>
2022-03-30 14:52:16 +0200

    created 2 buttons
    both work with link

---
+++ Assets/helloworld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    public void hello()
+    {
+        Debug.Log("Hello World");
+    }
+
+    public void bye()
+    {
+        Debug.Log("Goodbye World");
+    }
+
+
+
+}
+
