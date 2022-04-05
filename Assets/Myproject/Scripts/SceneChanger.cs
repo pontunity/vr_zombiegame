d4c30dd4ff1b8acd7ecae174efe62c03bcbe2f0f
pontunity <Pontus.Hogler@yh.nackademin.se>
2022-04-05 14:58:46 +0200

    removed button and fixed loadscene

--- Assets/Myproject/Scripts/SceneChanger.cs
+++ Assets/Myproject/Scripts/SceneChanger.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int nextscenetoload;
+
+    private void Start()
     {
-        
+        nextscenetoload = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        SceneManager.LoadScene(nextscenetoload);
     }
 }
