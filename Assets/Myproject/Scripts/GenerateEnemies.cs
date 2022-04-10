9b990324409438ae17c22eece5d5b3ddf46a6025
pontunity <Pontus.Hogler@yh.nackademin.se>
2022-04-10 21:29:49 +0200

    made generate enemies script and follow player
    works in link

---
+++ Assets/Myproject/Scripts/GenerateEnemies.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xpos;
+    public int zpos;
+    public int enemyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+        IEnumerator EnemyDrop()
+        {
+            while (enemyCount < 10)
+            {
+                xpos = Random.Range(-9, 15);
+                zpos = Random.Range(48, 55);
+                Instantiate(theEnemy, new Vector3(xpos, 0, zpos), Quaternion.identity);
+                yield return new WaitForSeconds(0.1f);
+                enemyCount += 1;
+            }
+        }
+    }
+
+
