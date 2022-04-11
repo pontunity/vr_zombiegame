931b67d16966ec65c0de459de8f9210c5a8ed1ca
pontunity <Pontus.Hogler@yh.nackademin.se>
2022-04-11 11:17:13 +0200

    fixed welcome screen UI
    pointer works
    hands shown,
    scenetransition works, user interface start button works.

---
+++ Assets/Myproject/Scripts/WelcomeSceneChange.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WelcomeSceneChange : MonoBehaviour
+{
+    public void SceneChange(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+}
